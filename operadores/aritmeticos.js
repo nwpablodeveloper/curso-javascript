4c8363a897e26f12721467dcb8876e352df42747
Pablo Veiga <nwpablodeveloper@gmail.com>
2023-09-28 12:28:29 +0200

    aritmeticos

--- operadores/aritmeticos.js
+++ operadores/aritmeticos.js
@@ -4,3 +4,47 @@
     y retornan un valor nemérico único
 
 */
+
+// --------------------------------------------------------
+
+let num1 = 50;
+let num2 = 10;
+
+let resultado = num1 + num2;  // Resultado 65
+let resultado2 = num1 - num2;  // Resultado 40
+
+// --------------------------------------------------------
+
+let num3 = 35;
+num3++ 
+// Resultado num3 = 36
+
+
+// --------------------------------------------------------
+
+let num4 = 40;
+alert(num4--); // Resultado 40
+
+
+let num5 = 40;
+num5--;
+alert(num5) // Resultado 39
+// debemos hacer la operación aritmetica antes de mostrar el valor
+
+let num6 = 40;
+resultado = num6++ // num6 va a seguir valiendo 40
+
+let num7 = 40;
+num7++;
+resultado = num7; // num7 = 41
+
+// --------------------------------------------------------
+
+let num8 = 25;
+let num9 = 7;
+
+resultado = num8 + num9; // Resultado 32
+resultado = num8 - num9; // Resultado 18
+resultado = num8 * num9; // Resultado 175
+resultado = num8 / num9; // Resultado 3.51...
+resultado2 = num8 % num9; // Resultado 4
--- operadores/asignacion.js
+++ operadores/asignacion.js
@@ -4,4 +4,43 @@
     de la derecha
 
 */
+// ----------------------------------------------
 
+let asignación = "Pepito";
+
+// Asgina un valor a la variable
+
+// ----------------------------------------------
+
+let adiccion = 10; 
+adiccion += 5; // Asignación de adicción 
+
+// Resultado 15 ( 10+5 )
+
+// ----------------------------------------------
+
+let sustraccion = 20;
+sustraccion -= 3; // Asignación de sustracción 
+
+// Resultado = 17 ( 20-3 )
+
+// ----------------------------------------------|
+
+let multiplicacion = 5;
+multiplicacion *= 4; //  Asignación de multiplicación 
+
+// Resultado = 20 ( 5*4 )
+
+// ----------------------------------------------
+
+let resto = 33;
+resto %= 10; // Asignación de resto  
+ 
+// Resultado = 3
+
+// ----------------------------------------------
+
+let exponente = 2;
+exponente **= 10;
+
+// Resultado = 1024 ( 2*2*2*2*2*2*2*2*2*2 )
