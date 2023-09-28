09c91d3f71ff307e9513c14f2afe61dddc86b633
Pablo Veiga <nwpablodeveloper@gmail.com>
2023-09-28 02:44:53 +0200

    variables

---
+++ Variables/variables.js
@@ -0,0 +1,9 @@
+// Tipo de dato String
+variable1 = "Esto es una variable con un tipo de dato string";
+
+// Tipo de dato Number
+variable2 = 1321321321;
+
+// Tipo de datos Boolean
+variable3 = true;
+variable4 = false;
